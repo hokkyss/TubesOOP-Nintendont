2e8b31e2e073f8e2b7fa0d287ac2b77e4914a93f
pege <mpg066@gmail.com>
2021-03-21 17:44:53 +0700

    Engimon, EngimonLiar, Position class untested

--- Engimon.hpp
+++ Engimon.hpp
@@ -1,7 +1,33 @@
 #ifndef ENGIMON_HPP
 #define ENGIMON_HPP
 
+#include "Skill.hpp"
+
 class Engimon {
+  private:
+    static int countID;
+    Skill skillUnique;
+    vector<string> elements;
+    vector<Skill> skills;
+    string name;
+    string species;
+    int idEngimon;
+    int level;
+    int exp;
+    int cumExp;
+    int maxExp;
+
+  public:
+    Engimon(string name, string species, vector<string> elements, int maxExp, Skill skillUnique);
+    ~Engimon();
+    void handleAddExp(int exp);
+    void Show();
+};
+
+class EngimonLiar:Engimon{
+  public:
+    pair <int,int> Position;
+    void randomMove();
 };
 
 #endif
