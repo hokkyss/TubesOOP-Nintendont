554b5f9f709f16c163c295a8256bfbbcf75d692d
hokkyss <hokkyss2@gmail.com>
2021-03-22 14:14:11 +0700

    unit testing skill

--- SkillTester.cpp
+++ SkillTester.cpp
@@ -1,6 +1,7 @@
 #include "Element.hpp"
 #include "Skill.hpp"
 #include "SkillItem.hpp"
+// #include "gtest/gtest.h"
 #include<iostream>
 #include<vector>
 // #include "Element.cpp"
@@ -12,6 +13,52 @@ using namespace std;
 // g++ -o nama_exe Element.cpp Skill.cpp SkillItem.cpp SkillTester.cpp
 // atau
 // g++ Element.cpp Skill.cpp SkillItem.cpp SkillTester.cpp -o nama_exe
+
+/*
+class SkillTester : public :: testing :: Test
+{
+	protected:
+		Skill s1 = Tackle;
+		Skill s2 = Bulldoze;
+		Skill s3 = ShockAndBurn;
+}
+TEST_F(SkillTester, Getter)
+{
+	EXPECT_EQ(s1.getBasePower(), 50);
+	EXPECT_EQ(s1.getMasteryLevel(), 0);
+	EXPECT_EQ(s1.getNumOfElements(), 5);
+	
+	EXPECT_EQ(s2.getBasePower(), 60);
+	EXPECT_EQ(s2.getMasteryLevel(), 0);
+	EXPECT_EQ(s2.getNumOfElements(), 1);
+	
+	EXPECT_EQ(s3.getBasePower(), 100);
+	EXPECT_EQ(s3.getMasteryLevel(), 0);
+	EXPECT_EQ(s3.getNumOfElements(), 2);
+}
+TEST_F(SkillTester, Setter)
+{
+	s1.setMasteryLevel(100);
+	EXPECT_EQ(s1.getMasteryLevel(), 100);
+}
+TEST_F(SkillTester, Exception)
+{
+	try
+	{
+		s1.getElement(6);
+		FAIL() << "Exception index invalid";
+	}
+	catch (exception& e)
+	{
+		EXPECT_EQ(e.what(), "Index invalid");
+	}
+}
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+*/
 
 int main()
 {
