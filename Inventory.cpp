b7546ea1b66a907103f7f47b4b83409216c05f4f
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-03-24 21:53:51 +0700

    add exception, minor change

--- Inventory.cpp
+++ Inventory.cpp
@@ -7,28 +7,23 @@ int FullInventory::currentCapacity = 0;
 
 template <class T>
 Inventory<T>::Inventory(){
-    maxCapacity = 15;
+    maxCapacity = MAX_CAPACITY;
 }
 
 template <class T>
 void Inventory<T>::remove(T el){
-    inventoryList.erase(find(el));
-}
-
-template <class T>
-int Inventory<T>::find(T el){
-    for(int i=0;i<inventoryList.size();i++){
-        if(inventoryList[i] == el) return i;
-    }
-    throw el;
-    return -1;
+    inventoryList.erase(find(inventoryList.begin(),inventoryList.end(),el));
 }
 
 template <class T>
 bool Inventory<T>::isExist(T el){
-    for(auto i=inventoryList.begin();i!=inventoryList.end();++i){
-        if(*i == el) return true;
-    }
+    return currentCapacity<maxCapacity;
+}
+
+template <class T>
+bool Inventory<T>::isExist(T el){
+    T elTemp = find(inventoryList.begin(),inventoryList.end(),el);
+    if (elTemp != inventoryList.end()) return true;
     return false;
 }
 
@@ -38,9 +33,9 @@ void Inventory<T>::insert(T in){
         if ((is_same<SkillItem,T>::value&&!isExist(in)) || is_same<Engimon,T>::value){
             inventoryList.push_back(in);
             currentCapacity++;
-        }else throw in;
+        }else throw ItemAlreadyExistedException();
     }
-    else throw maxCapacity;
+    else throw InventoryFullException();
 }
 
 template <class T>
@@ -51,6 +46,8 @@ void Inventory<T>::showInventory(){
     cout << ":" << endl;
 
     for(int i=0;i<inventoryList.size();i++){
-        cout << i+1 << ". " << inventoryList[i].nama << endl;
+        cout << i+1 << ". ";
+        if (is_same<SkillItem,T>::value) cout << inventoryList[i].getItemName() << endl;
+        else if (is_same<Engimon,T>::value) cout << inventoryList[i].getName() << endl;
     }
 }
--- Inventory.hpp
+++ Inventory.hpp
@@ -2,16 +2,43 @@
 #define INVENTORY_HPP
 
 #include <iostream>
+#include <exception>
 #include "Engimon.hpp"
 #include "SkillItem.hpp"
 #include "Skill.hpp"
 
 using namespace std;
 
+#define MAX_CAPACITY 15
+
+class ItemAlreadyExistedException : exception
+{
+	const char* what() const throw()
+	{
+		return "Item already owned!";
+	}
+};
+
+class InventoryFullException : exception
+{
+	const char* what() const throw()
+	{
+		return "Inventory is full!";
+	}
+};
+
+class EngimonNotFoundException : exception
+{
+	const char* what() const throw()
+	{
+		return "Engimon not found!";
+	}
+};
+
 class FullInventory{
     public:
         static int currentCapacity;
-        int maxCapacity = 15;
+        int maxCapacity;
 
         FullInventory(){}
         ~FullInventory(){}
@@ -25,8 +52,8 @@ class Inventory : public FullInventory{
         Inventory();
 
         //method
+        bool isFull();
         void remove(T el);
-        int find(T el);
         bool isExist(T el);
         void insert(T in);
         void showInventory();
