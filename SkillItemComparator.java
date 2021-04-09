0ef425c3a919a01aa175ddd7fe45b3876ea17a15
hokkyss <hokkyss2@gmail.com>
2021-04-09 17:41:22 +0700

    ubah comparator biar urutin dari terbesar

--- SkillItemComparator.java
+++ SkillItemComparator.java
@@ -1,9 +1,9 @@
 import java.util.Comparator;
 
-// PriorityQueue nya dari yang paling kecil
+// PriorityQueue nya dari yang paling besar
 // nanti tinggal PriorityQueue<SkillItem> nama = new PriorityQueue<>(new SkillItemComparator);
 public class SkillItemComparator implements Comparator<SkillItem> {
     public int compare(SkillItem si1, SkillItem si2) {
-        return si1.compare(si2);
+        return -si1.compare(si2);
     }
 }
