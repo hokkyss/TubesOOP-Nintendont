1b4347078b7e176d2addd556eb30a472b1f4f002
hokkyss <hokkyss2@gmail.com>
2021-04-10 10:55:23 +0700

    unit testing kosong

--- SkillItemComparator.java
+++ SkillItemComparator.java
@@ -1,7 +1,7 @@
 import java.util.Comparator;
 
 // PriorityQueue nya dari yang paling besar
-// nanti tinggal PriorityQueue<SkillItem> nama = new PriorityQueue<>(new SkillItemComparator);
+// nanti tinggal PriorityQueue<SkillItem> nama = new PriorityQueue<>(new SkillItemComparator());
 public class SkillItemComparator implements Comparator<SkillItem> {
     public int compare(SkillItem si1, SkillItem si2) {
         return -si1.compare(si2);
