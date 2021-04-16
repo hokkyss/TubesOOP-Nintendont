c0605c13a5b395ace41c4a0f01afe7fa4782fbce
hokkyss <hokkyss2@gmail.com>
2021-04-16 12:22:02 +0700

    element advantage

--- Element.java
+++ Element.java
@@ -1,14 +1,17 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public enum Element {
     FIRE(0), ICE(1), GROUND(2), ELECTRIC(3), WATER(4);
 
     public final int idx;
-    public final static ElementComparator comparator = new ElementComparator();
+    public static final ElementComparator comparator = new ElementComparator();
 
     private Element(int i) {
         this.idx = i;
     }
+
+    public static final double[][] advantage = { { 1, 0, 1, 0.5, 2 }, { 2, 1, 0, 1, 1 }, { 1, 2, 1, 0, 1.5 },
+            { 1.5, 1, 2, 1, 0 }, { 0, 1, 0.5, 2, 1 } };
 
     public static ArrayList<Element> constructElements(String elements) {
         ArrayList<Element> res = new ArrayList<Element>();
