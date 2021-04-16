48bf6af02631c0c15133eddc496f8626fa6d0294
hokkyss <hokkyss2@gmail.com>
2021-04-16 23:40:04 +0700

    first hasil setup

---
+++ LIBGDX/desktop/src/com/nintendont/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nintendont.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nintendont.EngimonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EngimonGame(), config);
+	}
+}
