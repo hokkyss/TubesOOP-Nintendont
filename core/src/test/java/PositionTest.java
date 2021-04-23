1afb75bb54f38399a7fb23805e4c687ee9f47d13
hokkyss <hokkyss2@gmail.com>
2021-04-23 12:34:31 +0700

    tambahin test utk position

--- core/src/test/java/PositionTest.java
+++ core/src/test/java/PositionTest.java
@@ -32,4 +32,20 @@ public class PositionTest {
         Assert.assertFalse(p3.equals(p2));
         Assert.assertTrue(p3.equals(p3));
     }
+
+    @Test
+    public void test_move()
+    {
+        Position p = new Position(0, 0);
+        Assert.assertEquals(0, p.getX());
+        Assert.assertEquals(0, p.getY());
+
+        p.move(2, 5);
+        Assert.assertEquals(2, p.getX());
+        Assert.assertEquals(5, p.getY());
+
+        p.move(-1, -2);
+        Assert.assertEquals(1, p.getX());
+        Assert.assertEquals(3, p.getY());
+    }
 }
